293564afb1567a8e28f6f3c3300210028c2fbe7d
Federico Ragona <federico.ragona@gmail.com>
2020-06-10 11:26:40 +0200

    Reset environment if last visited is missing or empty

--- examples/switchenvrc.fish
+++ examples/switchenvrc.fish
@@ -1,10 +1,16 @@
 #!/usr/local/bin/fish
 
-switch $PWD/
-  case "$switchenv_last_visited/*"
-    exit 0
-  case "*"
-    switchenv_add_ssh_identities ~/.ssh/my_id_rsa ~/.ssh/another_id_rsa
-    switchenv_append_to_user_path /some/path/ /some/other/path
-    switchenv_set_user_variable my_home /somewhere
+if test -n "$switchenv_last_visited"
+  switch $PWD/
+    case "$switchenv_last_visited/*"
+      exit 0
+    case "*"
+      switchenv_add_ssh_identities ~/.ssh/my_id_rsa ~/.ssh/another_id_rsa
+      switchenv_append_to_user_path /some/path/ /some/other/path
+      switchenv_set_user_variable my_home /somewhere
+  end
+else
+  switchenv_add_ssh_identities ~/.ssh/my_id_rsa ~/.ssh/another_id_rsa
+  switchenv_append_to_user_path /some/path/ /some/other/path
+  switchenv_set_user_variable my_home /somewhere
 end
