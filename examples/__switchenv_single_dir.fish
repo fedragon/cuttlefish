5de3c476ed2bc62fc795fc5b94cc22cc1c01315a
Federico Ragona <federico.ragona@gmail.com>
2020-07-18 20:34:08 +0200

    Reset internal state if system has been restarted
    by erasing all universal variables before doing anything else, so that customisations
    will be applied as usual

--- examples/__switchenv_single_dir.fish
+++ examples/__switchenv_single_dir.fish
@@ -1,6 +1,10 @@
 # scenario: we have only one "special" directory
 # goal: we want to reset the environment whenever we move out of it
 function __switchenv --on-variable PWD
+  if not test -f /tmp/switchenv
+    switchenv_erase
+  end
+
   switch $PWD/
     case "$HOME/personal/*"
       if test -z "$switchenv_last_visited"
@@ -13,4 +17,6 @@ function __switchenv --on-variable PWD
       switchenv_reset_user_path
       switchenv_reset_ssh_identities
   end
+
+  touch /tmp/switchenv
 end
